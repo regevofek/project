49828d28685dbb9ceed765553c149f4018725592
Regev Ofek <regev.s30@gmail.com>
2018-07-30 18:06:31 +0200

    Added bot

--- myBot.py
+++ myBot.py
@@ -1,48 +1,43 @@
-import random
 def do_turn(pw):
     if len(pw.my_planets()) == 0:
         return
-    if pw.turn_number()%4 == 1:
-        if len(pw.neutral_planets()) > 0:
-            all_neutral_planets = pw.neutral_planets()
-            tempName = []
-            for plant in all_neutral_planets:
-                tempName.append(plant.num_ships())
-            idx = tempName.index(min(tempName))
-            pw.issue_order(pw.my_planets()[random.randint(0,len(pw.my_planets())-1)], all_neutral_planets[idx], pw.my_planets()[0].num_ships()/2)
-
-        if pw.turn_number()>50:
-            if len(pw.enemy_planets()) >0:
-                enemy_planets = pw.enemy_planets()
-                ratio = []
-                for planet in enemy_planets:
-                    ratio.append(planet.num_ships())
-                index = ratio.index(min(ratio))
-
-                my_planets = pw.my_planets()
-                distance = []
-                for planet in my_planets:
-                    distance.append(pw.distance(planet, enemy_planets[index]))
-                attackers = []
-                try:
-                    for num in (0, 1, 2):
-                        i = min(distance)
-                        i = distance.index(i)
-                        attackers.append(my_planets[i])
-                        my_planets.remove(my_planets[i])
-                        distance.remove(distance[i])
-                    pw.issue_order(attackers[0], enemy_planets[index], attackers[0].num_ships()/3)
-                    pw.issue_order(attackers[1], enemy_planets[index], attackers[1].num_ships()/3)
-                    pw.issue_order(attackers[2], enemy_planets[index], attackers[2].num_ships()/3)
-                except:
-                    pw.issue_order(attackers[0], enemy_planets[index], attackers[0].num_ships()/3)
+    if pw.turn_number() > 68:
+        return
+    for my_planet in pw.my_planets():
+        playFirstTurn(my_planet, pw)
 
 
 
+def playFirstTurn(planet, pw):
+    num_planets_to_get = 5
+    nearbyPlanets = get_num_nearby_planets(planet, num_planets_to_get, pw)
+    remaining_ships = planet.num_ships()
+    for neutral_planet in nearbyPlanets:
+        shipsToSend = neutral_planet.num_ships() + 5
+        pw.debug(shipsToSend)
+        pw.debug(planet.num_ships())
+        if remaining_ships > shipsToSend:
+            pw.issue_order(planet, neutral_planet, shipsToSend)
+            remaining_ships -= shipsToSend
 
 
+def get_num_nearby_planets(planet, num, pw):
+    nearbyPlanets = list()
+    planetsList = list()
+    for neutral in pw.neutral_planets():
+        planetsList.append((pw.distance(planet, neutral), neutral))
 
+    for i in range(0, num, 1):
+        minDistance = 1000000
+        minIndex = 0
+        for x in range(0, len(planetsList), 1):
+            if planetsList[x][0] < minDistance:
+                minDistance = planetsList[x][0]
+                minIndex = x
+        nearbyPlanets.append(planetsList[minIndex][1])
+        planetsList.remove((planetsList[minIndex][0], planetsList[minIndex][1]))
 
+    return nearbyPlanets
 
 
 
